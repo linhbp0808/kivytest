45d78cc3a3e063925625de1df6095d3be8abe1c0
linhbp0808 <157904883+linhbp0808@users.noreply.github.com>
2024-02-05 19:32:34 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,21 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.clock import Clock
 import threading
 from datetime import datetime
+from telethon.sync import TelegramClient, events
+from telethon.errors import SessionPasswordNeededError
+import asyncio
+import time
+import threading
+api_id = 21624954
+api_hash = "aa76279b1489238c614ada49a21d62a3"
+proxy = {
+            'proxy_type': 'http',  # (mandatory) protocol to use (see above)
+            'addr': '42.117.216.248',  # (mandatory) proxy IP address
+            'port': 10663,  # (mandatory) proxy port number
+            'username': 'xoay',  # (optional) username if the proxy requires auth
+            'password': 'xoay',  # (optional) password if the proxy requires auth
+            'rdns': True
+            }
 
 class ClockApp(App):
     def build(self):
@@ -12,10 +27,21 @@ class ClockApp(App):
         layout.add_widget(self.label)
 
         # Sử dụng threading để cập nhật thời gian mà không làm đóng băng UI
-        threading.Thread(target=self.update_time_thread, daemon=True).start()
+        threading.Thread(target=self.laytinnhantele, daemon=True).start()
 
         return layout
+    def laytinnhantele(self):
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        client = TelegramClient('session_name', api_id, api_hash, proxy=proxy)
+        client.connect()
+        with client:
+            @client.on(events.NewMessage(from_users='tuyetbp0808'))
+            async def create_poll(event):
+                nds = event.message.message
+                Clock.schedule_once(lambda dt: self.update_label(nds))
 
+            client.run_until_disconnected()
     def update_time_thread(self):
         while True:
             current_time = datetime.now().strftime("%H:%M:%S")
