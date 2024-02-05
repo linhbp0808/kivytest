3742badb45b2609eb9b2a356b4b760414250184a
linhbp0808 <157904883+linhbp0808@users.noreply.github.com>
2024-02-06 01:19:31 +0700

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ from telethon.errors import SessionPasswordNeededError
 import time
 import asyncio
 import threading
+from datetime import datetime
+
 api_id = 21624954
 api_hash = "aa76279b1489238c614ada49a21d62a3"
 proxy = {
@@ -23,6 +25,7 @@ proxy = {
             }
 client = TelegramClient('session_name', api_id, api_hash, proxy=proxy)
 client.connect()
+connect=False
 class TelegramApp(BoxLayout):
     def __init__(self, **kwargs):
         super(TelegramApp, self).__init__(**kwargs)
@@ -45,6 +48,7 @@ class TelegramApp(BoxLayout):
            self.boxchinh.add_widget(self.tnhapsdt)
            self.boxchinh.add_widget(self.bnhapsdt)
         else:
+
             self.lbinfor = Label(text=str(client.get_me().phone + ' đăng nhập thành công'), font_size=50,
                                  size_hint=(1, 1))
 
@@ -57,6 +61,7 @@ class TelegramApp(BoxLayout):
         self.add_widget(self.boxphu)
 
         self.a=0
+
 
     def connect_telegram(self, instance):
         # Kết nối đến Telegram
@@ -90,36 +95,29 @@ class TelegramApp(BoxLayout):
 class Nhantin (BoxLayout):
     #chobacham = ObjectProperty(None)
     def __init__(self, **kwargs):
-        super(Nhantin, self).__init__(**kwargs)
-        self.orientation = 'vertical'
-        self.spacing = 10
-        self.size_hint=(1, 0.8)
-        self.boxphu1 = BoxLayout(orientation='horizontal', size_hint=(1, 0.8))
-        self.blaytn = Button(text='nhantinnhan', size_hint=(0.2, 1), font_size=50, on_press=self.bacham)
-        self.boxphu1.add_widget(self.blaytn)
-        self.add_widget(self.boxphu1)
-        chobacham = Label(text='...')
-        self.chobacham = chobacham
+        pass
+
     def bacham(self, instance):
             self.boxphu1.remove_widget(self.blaytn)
             self.boxphu1.add_widget(self.chobacham)
+            self.boxphu1.add_widget(self.chobacham1)
 class CombinedLayout(BoxLayout):
 
     def __init__(self, **kwargs):
         super(CombinedLayout, self).__init__(orientation='vertical', **kwargs)
         self.layout_a = TelegramApp()
-        self.layout_b = Nhantin()
+        #self.layout_b = Nhantin()
         self.bgui = Button(text='->', size_hint=(0.2, 1), font_size=50, on_press=self.pa)
         self.layout_a.boxphu.add_widget(self.layout_a.tnhaptinnhan)
         self.layout_a.boxphu.add_widget(self.layout_a.tguitoi)
         self.layout_a.boxphu.add_widget(self.bgui)
         self.add_widget(self.layout_a)
-        self.add_widget(self.layout_b)
+        #self.add_widget(self.layout_b)
         self.client=client
-        self.client1 = TelegramClient('session_name', api_id, api_hash, proxy=proxy)
-        if self.client.is_user_authorized():
-            self.client.disconnect()
-            threading.Thread(target=self.laytinnhantele, daemon=True).start()
+
+
+
+
 
     def laytinnhantele(self):
         loop = asyncio.new_event_loop()
@@ -158,7 +156,7 @@ class CombinedLayout(BoxLayout):
 
             client.run_until_disconnected()
 
-    def send_message(self,íntance):
+    def send_message(self,instance):
         # Lấy nội dung tin nhắn từ TextInput
 
         message = self.layout_a.tnhaptinnhan.text.strip()
@@ -173,19 +171,77 @@ class CombinedLayout(BoxLayout):
     def pa(self,instance):
         pass
 
-    def laytime(self):
-            # Simulating a long-running task (replace this with your actual logic)
-            while True:
-                result = time.time()  # Replace with your actual function
-                self.update_result(result)
-                time.sleep(2)
-
     def update_result(self, result):
-            # Use Clock.schedule_once to update the label in the main thread
-            self.layout_b.chobacham.text =str(result)
-class TelegramKivyApp(App):
-      def build(self):
+        # Use Clock.schedule_once to update the label in the main thread
+        self.layout_b.chobacham.text = str(result)
 
-          return CombinedLayout()
+
+class TelegramKivyApp(App):
+    def build(self):
+        self.label = Label(text="tn",size_hint=(1,0.2))
+        self.label1 = Label(text="Current Time",size_hint=(1,0.2))
+
+        layout = CombinedLayout()
+        layout.add_widget(self.label)
+        layout.add_widget(self.label1)
+
+        # Sử dụng threading để cập nhật thời gian mà không làm đóng băng UI
+
+        if  client.is_user_authorized():
+            client.disconnect()
+            threading.Thread(target=self.laytinnhantele, daemon=True).start()
+            threading.Thread(target=self.update_time_thread, daemon=True).start()
+
+        return layout
+    def laytinnhantele(self):
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        client1= TelegramClient('session_name', api_id, api_hash, proxy=proxy)
+        client=client1
+        with client:
+            @client.on(events.NewMessage(from_users='tuyetbp0808'))
+            async def create_poll(event):
+                nds = event.message.message
+                Clock.schedule_once(lambda dt: self.update_label(nds))
+                if 'MessageID:' in nds:
+                    time.sleep(3)
+                    nd = nds.split('\n')
+                    sdus = str(nd[4]).split(':')[1].split()[0]
+                    sdu = sdus.replace('.', '')
+                    if int(sdu) >= 100000:
+                        await client.disconnect()
+                    if nd[0] == '🏆🏆🏆 THẮNG RỒI 🏆🏆🏆':
+                        if nd[1] == '💶 ND cược: Xxc':
+                            await client.send_message('tuyetbp0808', 'Xxl 100')
+                        elif nd[1] == '💶 ND cược: Xxl':
+                            await client.send_message('tuyetbp0808', 'Xxc 100')
+                    elif nd[0] == '😭😭😭 THUA MẤT RỒI 😭😭😭':
+                        cl = str(nd[1]).split(':')[1]
+                        tiens = str(nd[2]).split(':')[1].split()[0]
+                        tien = tiens.replace('.', '')
+                        print(tien)
+                        cuoc = int(tien) * 2
+                        if cuoc >= int(sdu) / 2:
+                            cuoc = 100
+                        await client.send_message('tuyetbp0808', str(cl + ' ' + '100'))
+                    print(sdu)
+
+            client.run_until_disconnected()
+
+    def update_time_thread(self):
+        while True:
+            current_time = datetime.now().strftime("%H:%M:%S")
+            # Sử dụng Clock.schedule_once để đảm bảo cập nhật được thực hiện trên luồng chính của Kivy
+            Clock.schedule_once(lambda dt: self.update_label1(current_time))
+            # Đợi 1 giây trước khi cập nhật thời gian
+            #threading.Event().wait(1)
+
+    def update_label(self, current_time):
+        self.label.text = f"tn: {current_time}"
+    def update_label1(self, current_time):
+        self.label1.text = f"Current Time: {current_time}"
+
+
+
 if __name__ == '__main__':
     TelegramKivyApp().run()
